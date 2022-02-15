859716b4f4e847a5b92dd6cdc4c7781968bca7b3
anton <anton.romanova@ulb.be>
2022-02-15 13:27:06 +0100

    Added server index page

--- src/server_side/main.cpp
+++ src/server_side/main.cpp
@@ -1,26 +1,19 @@
 #define CROW_MAIN
 
 #include <iostream>
+#include "views/api/quoridor_api.h"
 #include "models/board.h"
 #include "models/position.h"
 #include "models/Game.h"
 
-#include "../includes/crow_all.hpp"
 
 
 int main() {
 
-  crow::SimpleApp app;
+  QuoridorAPI api;
 
-  CROW_ROUTE(app, "/")([]() {
-    return "Hello world";
-  });
+  api.Run();
 
-  CROW_ROUTE(app, "/great/<string>/")([](const std::string &name) {
-    return "Hello " + name;
-  });
-
-  app.port(18080).multithreaded().run();
 
 
   Game gameOn = Game(); //Normally this gets turned on by the server when the user wants to begin a game.
