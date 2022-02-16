da4d4310175049f49723324ce8995a353992b725
aybou17 <ayman.boulaich@ulb.be>
2022-02-16 23:42:39 +0100

    Login ReceiveMessage not finished

--- src/client_side/login.cpp
+++ src/client_side/login.cpp
@@ -6,8 +6,17 @@ void Login::SetUsername(string username){
 }
 
 void Login::setPassword(string password){
+    //vérification
     data[1] = password ;
 }
+int Login::ReceiveMessage(string message){
+    //10 == enter (normalement)
+    // fonction doit : verifier enter en fonction de où est le bouton et renvoyer enum
+    if (message == "10"){
+        return ClientMVCFactory::Login;
+    }
+}
+
 
 vector <string> Login::getData() const {
     if(data[0] != "" &&  data[1] != ""){
--- src/client_side/login.hpp
+++ src/client_side/login.hpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include "client_mvc_factory.h"
 using namespace std;
 
 class Login{
     vector <string> data={"",""};
 public:
+    int ReceiveMessage(string message);
     vector <string> getData() const ;
     void SetUsername(string username);
     void setPassword(string password);
