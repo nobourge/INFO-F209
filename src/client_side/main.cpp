30eec30af23eeace5833fb2c8a42273af370588e
anton <anton.romanova@ulb.be>
2022-02-21 19:22:31 +0100

    Added home_menu_view_controller to CMakeLists.txt + style fixes

--- src/client_side/main.cpp
+++ src/client_side/main.cpp
@@ -3,7 +3,7 @@
 #include "view_controllers/home_menu_view_controller.h"
 
 int main() {
-    std::optional<std::shared_ptr<AbstractViewController>> view_controller = std::make_shared<HomeMenuViewController>();
+  std::optional<std::shared_ptr<AbstractViewController>> view_controller = std::make_shared<HomeMenuViewController>();
   while (view_controller.has_value()) {
     view_controller = (*view_controller)->Tick();
   }
