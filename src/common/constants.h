a4359157cd0756610ae9791da68fbe4d9d5f632a
mosecund <mohammad.ik.secundar@ulb.be>
2022-02-25 00:46:04 +0100

    File name of Database included as a Constant in constants.h

--- src/common/constants.h
+++ src/common/constants.h
@@ -16,10 +16,14 @@
 #define MIN_PASSWORD_LENGTH               8
 #define MAX_PASSWORD_LENGTH               64
 
-// client-side
+// client_side
 #define MENU_NAME_EFFECT                  A_BOLD
 #define VIEW_HOVER_EFFECT                 A_REVERSE
 #define VIEW_SELECTED_EFFECT              A_UNDERLINE
 #define BUTTONS_VERTICAL_GUTTER_SIZE      1
+
+// server_side
+#define DATABASE_FILE_NAME                "example.db"
+
 
 #endif //QUORIDOR_SRC_COMMON_CONSTANTS_H_
