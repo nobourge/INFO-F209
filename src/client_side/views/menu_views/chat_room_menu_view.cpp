d7b568bda17786b13a403bdb8d37299dcdef52ef
mvdbremt <Mathieu.Van.Den.Bremt@ulb.be>
2022-02-27 17:13:53 +0100

    Modification ChatRoom

--- src/client_side/views/menu_views/chat_room_menu_view.cpp
+++ src/client_side/views/menu_views/chat_room_menu_view.cpp
@@ -6,10 +6,10 @@
 
 ChatRoomMenuView::ChatRoomMenuView() : AbstractMenuView(menu_name_) {
   UpdateSubviews({
-                     std::make_shared<Label>(this, "msg1"),
-                     std::make_shared<Label>(this, "msg2"),
-                     std::make_shared<Label>(this, "msg3"),
-                     std::make_shared<TextField>(this, "Text", this),
+                     std::make_shared<Label>(this, msg1),
+                     std::make_shared<Label>(this, msg2),
+                     std::make_shared<Label>(this, msg3),
+                     std::make_shared<TextField>(this, "Text:", this),
                      std::make_shared<MenuButtonItem>(this, "Back", std::optional<std::shared_ptr<AbstractViewController>>{}, this)
                  }
   );
@@ -18,6 +18,20 @@ ChatRoomMenuView::ChatRoomMenuView() : AbstractMenuView(menu_name_) {
 void ChatRoomMenuView::TextChanged(TextField &sender, const std::string &old_text) {
 }
 
+void ChatRoomMenuView::MessageChanged(std::string new_message) {
+  msg3=msg2;
+  msg2=msg1;
+  msg1=new_message;
+}
+
 void ChatRoomMenuView::TextEditingFinished(TextField &sender) {
-//  sender.SetInnerText("This TextField has been altered");
+  MessageChanged(sender.GetInnerText().substr(5,sender.GetInnerText().size()));
+  UpdateSubviews({
+                     std::make_shared<Label>(this, msg1),
+                     std::make_shared<Label>(this, msg2),
+                     std::make_shared<Label>(this, msg3),
+                     std::make_shared<TextField>(this, "Text:", this),
+                     std::make_shared<MenuButtonItem>(this, "Back", std::optional<std::shared_ptr<AbstractViewController>>{}, this)
+                 }
+  );
 }
--- src/client_side/views/menu_views/chat_room_menu_view.h
+++ src/client_side/views/menu_views/chat_room_menu_view.h
@@ -13,10 +13,14 @@ class ChatRoomMenuView: public AbstractMenuView, public TextFieldDelegate {
  public:
   void TextChanged(TextField &sender, const std::string &old_text) override;
   void TextEditingFinished(TextField &sender) override;
+  void MessageChanged(std::string new_message);
   ChatRoomMenuView();
  private:
   std::shared_ptr<TextField>text_field_;
   constexpr static const char menu_name_[] = "Chat Room Menu";
+  std::string msg1="msg1";
+  std::string msg2="msg2";
+  std::string msg3="msg3";
 };
 
 #endif //INFO_F209_SRC_CLIENT_SIDE_VIEWS_MENU_VIEWS_VIEWS_CHAT_ROOM_MENU_VIEW_H_
