94a69097165023e352d5eded03eb22c8627e25ab
mosecund <mohammad.ik.secundar@ulb.be>
2022-03-06 16:24:08 +0100

    Files deleted because not used

--- src/client_side/view_controllers/selection_menu_view_controller.cpp
+++
@@ -1,6 +0,0 @@
-
-#include "selection_menu_view_controller.h"
-
-
-SelectionMenuViewController::SelectionMenuViewController()
-    : MenuViewController(std::make_shared<SelectionMenuView>()){}//TODO need to be corrected
