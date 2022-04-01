518ea689e7e2f314801c94028438584c403f7bb4
EstAk <aguiklein@hotmail.com>
2022-04-01 22:25:53 +0200

    single cmake

--- src/client_side/QtCode/game/assets.h
+++ src/client_side/QtCode/game/assets.h
@@ -11,11 +11,10 @@
 #include <cstring>
 #include <iostream>
 
-const std::string cell_png = "../QtCode/game/assets/cell.png";
-const std::string no_wall_png = "../QtCode/game/assets/noWall.png";
-//const std::string pawn_png = "../QtCode/game/assets/pawn.png";
-const std::string pawn_png = "../QtCode/game/assets/pawn_red.png";
-const std::string wallHorizontal_png = "../QtCode/game/assets/wallHorizontal.png";
-const std::string wallVertical_png = "../QtCode/game/assets/wallVertical.png";
+const std::string cell_png = "src/client_side/QtCode/game/assets/cell.png";
+const std::string no_wall_png = "src/client_side/QtCode/game/assets/noWall.png";
+const std::string pawn_png = "src/client_side/QtCode/game/assets/pawn_red.png";
+const std::string wallHorizontal_png = "src/client_side/QtCode/game/assets/wallHorizontal.png";
+const std::string wallVertical_png = "src/client_side/QtCode/game/assets/wallVertical.png";
 
 #endif //QUORIDOR_SRC_CLIENT_SIDE_QTCODE_ASSETS_H_
