ac1f721f474594c8d1d84df3e9de3067c1e42f6a
EstAk <aguiklein@hotmail.com>
2022-04-01 22:56:36 +0200

    each player has a color

--- src/client_side/QtCode/game/assets.h
+++ src/client_side/QtCode/game/assets.h
@@ -13,7 +13,8 @@
 
 const std::string cell_png = "src/client_side/QtCode/game/assets/cell.png";
 const std::string no_wall_png = "src/client_side/QtCode/game/assets/noWall.png";
-const std::string pawn_png = "src/client_side/QtCode/game/assets/pawn_red.png";
+const std::string pawn_png = "src/client_side/QtCode/game/assets/pawn.png";
+const std::string red_pawn_png = "src/client_side/QtCode/game/assets/pawn_red.png";
 const std::string wallHorizontal_png = "src/client_side/QtCode/game/assets/wallHorizontal.png";
 const std::string wallVertical_png = "src/client_side/QtCode/game/assets/wallVertical.png";
 
